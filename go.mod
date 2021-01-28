ce8bbcd2a713b9bfe00f34ae6c5c40d92526b9da
funa1g <funa1g@gmail.com>
2021-01-28 15:11:27 +0900

    [wip] add handler test code

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.15
 require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/mock v1.4.4
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/rs/zerolog v1.20.0
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
--- pkg/petshop/delivery/http/pet_handler_test.go
+++ pkg/petshop/delivery/http/pet_handler_test.go
@@ -1,7 +1,50 @@
 package http_test
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	delivery "github.com/funa1g/microservice-example/pkg/petshop/delivery/http"
+	"github.com/funa1g/microservice-example/pkg/petshop/domain"
+	mock_domain "github.com/funa1g/microservice-example/pkg/petshop/domain/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestGetList(t *testing.T) {
+	ctrl := gomock.NewController(t)
 
+	// Assert that Bar() is invoked.
+	defer ctrl.Finish()
+
+	limit := 10
+	e := echo.New()
+	req, err := http.NewRequest(echo.GET, "/pets/?limit="+strconv.Itoa(limit), strings.NewReader(""))
+	assert.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	con := c.Request().Context()
+
+	m := mock_domain.NewMockPetUsecase(ctrl)
+	pets := []domain.Pet{
+		domain.Pet{
+			ID:   1,
+			Name: "pochi",
+			Tag:  "dog",
+		},
+	}
+	m.EXPECT().GetList(con, limit).Return(pets, nil)
+
+	handler := &delivery.PetHandler{
+		PUsecase: m,
+	}
+	err = handler.GetList(c)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
 }
