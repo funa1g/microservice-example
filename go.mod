fd1ec777d570f9545158bce5fa171853c34f35a8
funa1g <funa1g@gmail.com>
2021-02-01 12:01:13 +0900

    implement grpc protocol

--- go.mod
+++ go.mod
@@ -6,12 +6,20 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/mock v1.4.4
+	github.com/golang/protobuf v1.4.3
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/rs/zerolog v1.20.0
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506 // indirect
+	google.golang.org/grpc v1.35.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.25.0
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
---
+++ pkg/petshop/delivery/grpc/pet_handler.go
@@ -0,0 +1,60 @@
+package grpc
+
+import (
+	"context"
+
+	pb "github.com/funa1g/microservice-example/pkg/petshop/delivery"
+	"github.com/funa1g/microservice-example/pkg/petshop/domain"
+)
+
+type PetHandler struct {
+	pb.UnimplementedPetshopServer
+	PUsecase domain.PetUsecase
+}
+
+func (p *PetHandler) GetPetList(ctx context.Context, req *pb.GetPetListRequest) (*pb.PetListResponse, error) {
+	pets, err := p.PUsecase.GetList(ctx, int(req.GetLimit()))
+	if err != nil {
+		return nil, err
+	}
+
+	res := &pb.PetListResponse{}
+	for _, pet := range pets {
+		pr := convertPetToPetResponse(pet)
+		res.Results = append(res.Results, pr)
+	}
+	return res, nil
+}
+
+func (p *PetHandler) PostPet(ctx context.Context, req *pb.PostPetRequest) (*pb.PetResponse, error) {
+	pet := &domain.Pet{
+		Name: req.GetName(),
+		Tag:  req.GetTag(),
+	}
+	res, err := p.PUsecase.Store(ctx, pet)
+	if err != nil {
+		return nil, err
+	}
+
+	result := convertPetToPetResponse(res)
+	return result, nil
+}
+
+func (p *PetHandler) GetPetById(ctx context.Context, req *pb.GetPetRequest) (*pb.PetResponse, error) {
+	pet, err := p.PUsecase.GetById(ctx, int(req.GetId()))
+	if err != nil {
+		return nil, err
+	}
+
+	result := convertPetToPetResponse(pet)
+
+	return result, nil
+}
+
+func convertPetToPetResponse(pet domain.Pet) *pb.PetResponse {
+	return &pb.PetResponse{
+		Id:   int32(pet.ID),
+		Name: pet.Name,
+		Tag:  pet.Tag,
+	}
+}
